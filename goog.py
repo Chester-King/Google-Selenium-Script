c5daeb00c396da138a92bb24c6ba88e445b79b17
Madhur Dixit <dmadhur6@gmail.com>
2019-01-21 17:25:02 +0530

    Browser automation for VOLSBB login

--- goog.py
+++ goog.py
@@ -1,8 +1,26 @@
 import time
+from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
 
 driver = webdriver.Chrome("D:/Work/Browser Automation/chromedriver.exe")
-driver.get("https://www.google.com")
+# driver.get("https://www.google.com")
+driver.get("http://gstatic.com/generate_204")
+
+try:
+    time.sleep(2)
+    elem=driver.find_element_by_name('userId')
+    print('Found reg entery')
+    elem.send_keys("<Reg. Number>")
+    time.sleep(2)
+    elem=driver.find_element_by_name('password')
+    time.sleep(2)
+    elem.send_keys("<Password>")
+    elem.send_keys(Keys.ENTER)
+except Exception as e:
+    pass
+
+
+
 print(driver.title)
 time.sleep(8)
 driver.quit
